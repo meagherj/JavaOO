e51d04a023cea20521c8ff4cd0a4d0ce9536fa4b
Arjun-Subra <97113551+Arjun-Subra@users.noreply.github.com>
2022-12-01 08:51:33 -0600

    Update Parent.java

--- src/org/gh/Parent.java
+++ src/org/gh/Parent.java
@@ -1,1 +1,24 @@
+package org.gh;
 
+public class Teacher extends Person implements Printable{
+
+    private String classes;
+
+    public Teacher(String name, String students){
+        super (name);
+
+        this.setStudents(students);
+    }
+
+    public String getStudents(){
+        return this.students;
+    }
+
+    public void setStudents(String students){
+        this.classes = students;
+    }
+
+    public String printMe(){
+        return "Teacher details from printMe() = " + this.getName()+ " - "+this.getClasses();
+    }
+}
