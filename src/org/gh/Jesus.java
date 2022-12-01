4bd6c47d3b01abbc9d802d6ae8b58d3e06b90006
Cwilliams68419 <116006731+Cwilliams68419@users.noreply.github.com>
2022-12-01 08:50:50 -0600

    Jesusv2

--- src/org/gh/Jesus.java
+++ src/org/gh/Jesus.java
@@ -1,1 +1,29 @@
+//  Think of package as a directory, we can import packages for reuse
+package org.gh;
 
+
+public class Jesus extends Person implements Printable{
+
+
+    private String books;
+ 
+    public void setfavbook(String books){
+        this.books = books;
+    }
+
+    public String getfavbook(){
+        return this.books;
+    }
+    
+ 
+    public Jesus(String name, String book){
+        super(name);
+        this.setfavbook(books);
+    }
+
+
+    public String printMe(){
+
+        return "Student details from PrintMe() - " + this.getName()+ " - "+this.getfavbooks();
+    }
+}
