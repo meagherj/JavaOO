e81d4dbb6d93ec1a097381006788473148e86052
actual poop <116006806+poop-lover@users.noreply.github.com>
2022-12-01 08:48:11 -0600

    Update PK.java

--- src/org/gh/PK.java
+++ src/org/gh/PK.java
@@ -4,15 +4,19 @@ public class PK extends Person implements Printable{
 
     private String pokemonCaught;
     
-    public String getpokemonCaught(){
-        return this.pokemonCaught;
-    }
-    
-    public PK(String name, String pokemonCaught){
-        super(name);
+    public void setPokemonCaught(String pokemonCaught){
         this.pokemonCaught = pokemonCaught;
     }
 
+    public String getpokemonCaught(){
+        return this.pokemonCaught;
+    }
+
+    public PK(String name, String pokemonCaught){
+        super(name);
+        this.setPokemonCaught(pokemonCaught);
+    }
+    
     public String printMe(){
         return "Pokemon Caught - " + this.getName()+ ", "+this.getpokemonCaught();
     }
