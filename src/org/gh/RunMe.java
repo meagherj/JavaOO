f4509360fe4d356a51cd545f9735897b464ed274
smithd23 <66260633+smithd23@users.noreply.github.com>
2022-12-01 09:27:33 -0600

    Update RunMe.java

--- src/org/gh/RunMe.java
+++ src/org/gh/RunMe.java
@@ -11,7 +11,7 @@ public class RunMe{
         // see getName() and toString() below which are defined in Person
         System.out.println("Creating a new student and Teacher");
         Student objStudent = new Student("Student 1", "GradeA");
-        Senior objSenior = new Senior("Senior 1", "gpa");
+        Senior objSenior = new Senior("Senior 1", "Gpa1");
         Teacher objTeacher = new Teacher("Teacher 1", "Class#1, Class#2");
 
 
