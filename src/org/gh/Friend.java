fd764928bb29339c0a841540a02e62cb762c4599
nehabachu <89409075+nehabachu@users.noreply.github.com>
2022-12-01 08:50:52 -0600

    Create Friend.java

---
+++ src/org/gh/Friend.java
@@ -0,0 +1,34 @@
+//  Think of package as a directory, we can import packages for reuse
+package org.gh;
+
+// extends Parent means we inherit from Parent
+// This gives Student the ability to use Parent methods and attributes
+// implements Printable means we must override the methods in that interface
+public class Friend extends Person implements Printable{
+
+    // attribute to store students age 
+    // NOTE: it's private, so we provide a getter and setter
+    private String age;
+    
+    // set age
+    public void setAge(String age){
+        this.age = age;
+    }
+    // get age
+    public String setAge(){
+        return this.age;
+    }
+    
+    // constructor called to create a new student object
+    //  See it's usage in RunMe
+    public Student(String name, String age){
+        super(name);
+        this.setAge(age);
+    }
+
+    // Because this class implements Printable, I must override the printMe() method
+    public String printMe(){
+        // For a student, I want the name and age to print
+        return "Student details from PrintMe() - " + this.getName()+ " - "+this.getAge();
+    }
+}
