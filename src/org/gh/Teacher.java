968ced26f0ad4c754bf7d404b2f510677df8985f
Timo K <79548141+DIO-sys@users.noreply.github.com>
2022-12-01 09:11:21 -0600

    Update Teacher.java

--- src/org/gh/Teacher.java
+++ src/org/gh/Teacher.java
@@ -2,34 +2,34 @@
 package org.gh;
 
 // extends Parent means we inherit from Parent
-// This gives Teacher the ability to use Parent methods and attributes
+// This gives Advisor the ability to use Parent methods and attributes
 // implements Printable means we must override the methods in that interface
-public class Teacher extends Person implements Printable{
+public class Advisor extends Person implements Printable{
 
-    // attribute to store teacher's class
-    private String classes;
+    // attribute to store advisor's class
+    private String folder;
 
     // Constructor of tacher taking a name and class as input
-    public Teacher(String name, String classes){
+    public Adivsor(String name, String folder){
         // Call superclass constructor (in this case Person)
         super (name);
 
-        // Set the class for this teacher
-        this.setClasses(classes);
+        // Set the class for this advisor
+        this.setFolder(folder);
     }
 
     // get Class
-    public String getClasses(){
-        return this.classes;
+    public String getFolder(){
+        return this.folder;
     }
 
     // Set class
-    public void setClasses(String classes){
-        this.classes = classes;
+    public void setFolder(String folder){
+        this.folder = folder;
     }
 
     // Because this class implements Printable, I must override the printMe() method
     public String printMe(){
-        return "Teacher details from printMe() = " + this.getName()+ " - "+this.getClasses();
+        return "Advisor details from printMe() = " + this.getName()+ " - "+this.getFolder();
     }
 }
