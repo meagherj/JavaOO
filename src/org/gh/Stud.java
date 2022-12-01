d6c06726cd2116ae68b6a3d1f5a28230f34daa89
titush6002 <116006591+titush6002@users.noreply.github.com>
2022-12-01 09:05:57 -0600

    Update Stud.java

--- src/org/gh/Stud.java
+++ src/org/gh/Stud.java
@@ -4,32 +4,32 @@ package org.gh;
 // extends Parent means we inherit from Parent
 // This gives Student the ability to use Parent methods and attributes
 // implements Printable means we must override the methods in that interface
-public class Stud extends Person implements Printable{
+public class McdWork extends Person implements Printable{
 
     // attribute to store students grade 
     // NOTE: it's private, so we provide a getter and setter
-    private String grade;
+    private String pay;
     
     // set grade
-    public void setGrade(String grade){
-        this.grade = grade;
+    public void setPay(String pay){
+        this.pay = pay;
     }
     // get grade
-    public String getGrade(){
-        return this.grade;
+    public String getPay(){
+        return this.pay;
     }
     
     // constructor called to create a new student object
     //  See it's usage in RunMe
-    public Stud(String name, String grade){
+    public McdWork(String name, String grade){
         super(name);
-        this.setGrade(grade);
+        this.setPay(pay);
     }
 
     // Because this class implements Printable, I must override the printMe() method
     public String printMe(){
         // For a student, I want the name and grade to print
-        return "Student details from PrintMe() - " + this.getName()+ " - "+this.getGrade();
+        return "McdWork details from PrintMe() - " + this.getName()+ " - "+this.getPay();
     }
 }
 //1
