7bb8a60b0d4e5717de041e6f9ff2b02cbea323e2
smithd23 <66260633+smithd23@users.noreply.github.com>
2022-12-05 08:40:27 -0600

    Update RunMe.java

--- src/org/gh/RunMe.java
+++ src/org/gh/RunMe.java
@@ -11,20 +11,22 @@ public class RunMe{
         System.out.println("Creating a new student and Teacher");
         Student objStudent = new Student("Student 1", "GradeA");
         Teacher objTeacher = new Teacher("Teacher 1", "Class#1, Class#2");
-        Senior objSenior = new Senior("Senior 1", "Gpa2.0");
+        Senior objSenior = new Senior("Senior", "Gpa #");
 
         // Print details of a student we created above (name - grade)
         System.out.println("Print with name (from person) and grade (from student)");
         System.out.println(objStudent.getName() + " - " + objStudent.getGrade());
-        
-        System.out.println("Print with name (from person) and gpa (from senior)");
-        System.out.println(objSenior.getName() + " - " + objSenior.getGpa());
-        
+
 
         // Print details of a Teacher we created above (name - classes)
         System.out.println("Print with name (from person) and Classes (from Teacher)");
         System.out.println(objTeacher.getName() + " - " + objTeacher.getClasses());
-            
+        
+        
+        
+        System.out.println("Print with name (from person) and Classes (from Senior)");
+        System.out.println(objSenior.getName() + " - " + objSenior.getGpa());
+        
 
         // This uses the polymorphism to call printMe and see different results
         System.out.println("Print the Student and teacher using the printMe method from Printable");
