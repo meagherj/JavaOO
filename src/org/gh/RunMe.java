dc509a2523a610a161838a00f631548bbe2712ae
smithd23 <66260633+smithd23@users.noreply.github.com>
2022-12-05 09:07:18 -0600

    Update RunMe.java

--- src/org/gh/RunMe.java
+++ src/org/gh/RunMe.java
@@ -11,7 +11,7 @@ public class RunMe{
         System.out.println("Creating a new student and Teacher");
         Student objStudent = new Student("Student 1", "GradeA");
         Teacher objTeacher = new Teacher("Teacher 1", "Class#1, Class#2");
-        senior objSenior = new Senior("Senior", "Gpa #");
+        Senior objSenior = new Senior("Senior", "Gpa #");
 
         // Print details of a student we created above (name - grade)
         System.out.println("Print with name (from person) and grade (from student)");
